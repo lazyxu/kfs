15717e04d44f8b00f5284609e25362cbe54fb8fa
Xu Liang <isxuliang@gmail.com>
2021-01-05 23:26:53 +0800

    make Modal dragable

--- ui/src/components/Modal.jsx
+++ ui/src/components/Modal.jsx
@@ -9,13 +9,8 @@ const Modal = styled.div`
   display: grid;
   grid-template-columns: 1fr;
   grid-template-rows: auto 1fr auto;
-  max-width: calc(100% - 5em);
-  max-height: calc(100% - 5em);
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  margin: auto;
+  max-width: calc(100% - 100px);
+  max-height: calc(100% - 100px);
   border-radius: var(--modal-radius);
   border: 1px solid #717171;
   background: var(--modal-body-color);
@@ -42,35 +37,76 @@ const Text = styled.div`
   text-align: center;
 `;
 
-export default class component extends React.Component {
-  render() {
-    const {
-      isOpen, disableSave, save, close, children, title, content, ...props
-    } = this.props;
-    return (
-      <Modal
-        {...props}
-        style={Object.assign(props.style || {}, { display: isOpen ? 'grid' : 'none' })}
-        onKeyDown={e => { e.stopPropagation(); }}
+export default React.memo(({
+  isOpen, disableSave, save, close, children, title, content, ...props
+}) => {
+  const cur = React.useRef();
+  const [pos, setPos] = React.useState({ top: 50, left: 50 });
+  const [size, setSize] = React.useState({ width: 800, height: 600 });
+  const onMove = e => {
+    if (cur.current) {
+      const { clientX, clientY } = e;
+      const { clientWidth, clientHeight } = document.documentElement;
+      if (clientX < 0 || clientX > clientWidth || clientY < 0 || clientY > clientHeight) {
+        cur.current = undefined;
+        document.removeEventListener('mousemove', onMove);
+        return;
+      }
+      const offX = clientX - cur.current.clientX;
+      const offY = clientY - cur.current.clientY;
+      if (offX === 0 || offY === 0) {
+        return;
+      }
+      setPos((prev) => {
+        return {
+          top: offY + prev.top,
+          left: offX + prev.left,
+        };
+      });
+      cur.current = { clientX, clientY };
+    }
+  };
+  return (
+    <Modal
+      {...props}
+      style={Object.assign(props.style || {}, {
+        display: isOpen ? 'grid' : 'none',
+        top: `${pos.top}px`,
+        left: `${pos.left}px`,
+        width: `${size.width}px`,
+        height: `${size.height}px`,
+      })}
+      onKeyDown={e => { e.stopPropagation(); }}
+    >
+      <Header
+        onMouseDown={e => {
+          cur.current = { clientX: e.clientX, clientY: e.clientY };
+          document.removeEventListener('mousemove', onMove);
+          document.addEventListener('mousemove', onMove);
+        }}
+        onMouseUp={e => {
+          if (cur.current) {
+            document.removeEventListener('mousemove', onMove);
+            cur.current = undefined;
+          }
+        }}
       >
-        <Header>
-          <Text>
-            {title}
-          </Text>
-          <Empty />
-          <Icon
-            icon="close"
-            color="#cccccc"
-            size="1.5em"
-            hoverColor="white"
-            hoverCursor="pointer"
-            onClick={() => { close(); }}
-          />
-        </Header>
-        <Body>
-          {children}
-        </Body>
-      </Modal>
-    );
-  }
-}
+        <Text>
+          {title}
+        </Text>
+        <Empty />
+        <Icon
+          icon="close"
+          color="#cccccc"
+          size="1.5em"
+          hoverColor="white"
+          hoverCursor="pointer"
+          onClick={() => { close(); }}
+        />
+      </Header>
+      <Body>
+        {children}
+      </Body>
+    </Modal>
+  );
+});
