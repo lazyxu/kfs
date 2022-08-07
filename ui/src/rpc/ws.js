8974bfa45e280926ea7c086107bad24059de9c41
Xu Liang <isxuliang@gmail.com>
2022-08-07 22:05:09 +0800

    WebSocketReceiver

--- ui/src/rpc/ws.js
+++ ui/src/rpc/ws.js
@@ -4,53 +4,59 @@
 import { getConfig } from './config';
 import { PathReq, DirItem } from '../pb/fs_pb';
 
-async function wait(fn) {
-  return await new Promise((resolve, reject) => {
-    setInterval(() => {
-      if (fn()) {
-        resolve();
-      }
-    }, 100);
-  });
+class WebSocketReceiver {
+  constructor(ws) {
+    this.ws = ws;
+    this.nextPromise = this.getNextPromise();
+  }
+  getNextPromise() {
+    let that = this;
+    let cur = new Promise((resolve, reject)=> {
+      this.ws.addEventListener('message', ({ data }) => {
+        data.arrayBuffer().then(bytes => {
+          resolve(bytes);
+        });
+        cur.next = that.getNextPromise();
+      }, { once: true });
+    });
+    return cur;
+  }
+  async recv() {
+    let data = await this.nextPromise;
+    this.nextPromise = this.nextPromise.next;
+    return data;
+  }
 }
 
 export function list() {
-  let p = {};
-  const rws = new WebSocket(getConfig().wsHost);
-  rws.addEventListener('open', async () => {
-    rws.send(new Uint8Array([1]));
+  const ws = new WebSocket(getConfig().wsHost);
+  ws.addEventListener('open', async () => {
+    ws.send(new Uint8Array([1]));
     let req = new PathReq();
     console.log(req);
     req.setBranchname("master");
     req.setPath("/");
     let reqData = req.serializeBinary();
     console.log(reqData);
-    rws.send(new Int32Array([reqData.length, 0]));
-    rws.send(reqData);
-    await wait(() => { return p.p; });
-    let data = await p.p;
-    p = p.p;
+    ws.send(new Int32Array([reqData.length, 0]));
+    ws.send(reqData);
+
+    let receiver = new WebSocketReceiver(ws);
+    let data = await receiver.recv();
     let code = new Int8Array(data)[0];
-    console.log('code', code);
-    data = await p.p;
-    p = p.p;
+    console.log('code', data, code);
+    data = await receiver.recv();
     let total = new Int32Array(data)[0];
-    console.log('total', total);
+    console.log('total', data, total);
     for (let i = 0; i < total; i++) {
-      data = await p.p;
-      p = p.p;
+      data = await receiver.recv();
       console.log('length', new Int32Array(data)[0]);
-      data = await p.p;
-      p = p.p;
+      data = await receiver.recv();
       let resp = DirItem.deserializeBinary(data);
       console.log('resp', resp.toObject());
     }
-  });
-  let lastP = p;
-  rws.addEventListener('message', ({ data }) => {
-    lastP.p = (async () => {
-      return await data.arrayBuffer();
-    })()
-    lastP = lastP.p;
+    data = await receiver.recv();
+    code = new Int8Array(data)[0];
+    console.log('exit code', code);
   });
 }
