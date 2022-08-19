e293b2a9dad9adcc28921fe3c0a7329b514db7fe
Xu Liang <isxuliang@gmail.com>
2022-08-20 00:39:30 +0800

    off no-unused-vars

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -24,7 +24,7 @@ module.exports = {
         // Customize your rules
         "max-params": "off",
         "prefer-arrow-callback": "off",
-        "no-unused-vars": "warn",
+        "no-unused-vars": "off",
         "react/no-unescaped-entities": "off"
     },
 };
