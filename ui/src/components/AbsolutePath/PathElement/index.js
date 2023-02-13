8ae524941b7e305502cca22df47f1a46535e6351
Xu Liang <isxuliang@gmail.com>
2023-02-14 02:19:46 +0800

    ui for branch in PathElement

--- ui/src/components/AbsolutePath/PathElement/index.js
+++ ui/src/components/AbsolutePath/PathElement/index.js
@@ -2,16 +2,31 @@ import useResourceManager from 'hox/resourceManager';
 import {useClick} from "use";
 import {open} from "api/api";
 import useSysConfig from "hox/sysConfig";
+import {Link, Stack} from "@mui/material";
+import SvgIcon from '@mui/material/SvgIcon';
 
-export default ({ name, filePath, separator }) => {
+export default ({name, icon, filePath}) => {
     const [resourceManager, setResourceManager] = useResourceManager();
     const {sysConfig} = useSysConfig();
     return (
-        <div className='pathElement' onMouseDown={useClick(() => {
+        <Link color="inherit" underline="hover" onMouseDown={useClick(() => {
             open(sysConfig, setResourceManager, resourceManager.branchName, filePath);
         })}>
-            <div className='pathName'>{name}</div>
-            <div className='pathNameRight'>{separator}</div>
-        </div>
+            <Stack direction="row"
+                   justifyContent="flex-start"
+                   alignItems="center"
+            >
+                {icon && <SvgIcon color="inherit" fontSize="inherit">
+                    <svg
+                        aria-hidden="true"
+                        viewBox="0 0 200 200"
+                        preserveAspectRatio="xMinYMin meet"
+                    >
+                        <use xlinkHref={`#icon-${icon}`}/>
+                    </svg>
+                </SvgIcon>}
+                {name}
+            </Stack>
+        </Link>
     )
 };
--- ui/src/components/AbsolutePath/index.js
+++ ui/src/components/AbsolutePath/index.js
@@ -1,16 +1,31 @@
 import useResourceManager from 'hox/resourceManager';
 import './index.scss';
 import PathElement from "./PathElement";
+import Icon from "../Icon/Icon";
+import {Breadcrumbs, Link, Stack} from "@mui/material";
+import {open} from "../../api/api";
+import useSysConfig from "../../hox/sysConfig";
 
 export default () => {
-    const [resourceManager] = useResourceManager();
+    const [resourceManager, setResourceManager] = useResourceManager();
+    const {sysConfig} = useSysConfig();
     return (
-        <div className='filePath'>
-            <PathElement name={resourceManager.branchName} filePath={[]} separator="$"/>
-            {resourceManager.filePath.map((elemName, i) =>
-                <PathElement key={i} name={elemName} filePath={resourceManager.filePath.slice(0, i + 1)}
-                             separator={i === resourceManager.filePath.length - 1 ? "" : ">"}/>
-            )}
-        </div>
+        <Stack className='filePath'
+               direction="row"
+               justifyContent="flex-start"
+               alignItems="center"
+               spacing={1}
+        >
+            <Link onClick={() => {
+                open(sysConfig, setResourceManager, resourceManager.branchName, []);
+            }} size="small"><Icon icon="wangpan"/></Link>
+            <div style={{color: "rgba(255, 255, 255, 0.7)"}}> ></div>
+            <Breadcrumbs separator="/" maxItems={5}>
+                <PathElement name={resourceManager.branchName} filePath={[]} icon="git"/>
+                {resourceManager.filePath.map((elemName, i) =>
+                    <PathElement key={i} name={elemName} filePath={resourceManager.filePath.slice(0, i + 1)}/>
+                )}
+            </Breadcrumbs>
+        </Stack>
     )
 };
