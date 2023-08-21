497c4fb4fcb8d2a2398eb5e3b57f8c8ee060202d
Xu Liang <isxuliang@gmail.com>
2023-08-21 21:47:50 +0800

    exif.Orientation

--- ui/src/pages/Dcim/Image.jsx
+++ ui/src/pages/Dcim/Image.jsx
@@ -2,8 +2,32 @@ import { parseShotTime } from 'api/utils/api';
 
 export default function ({ hash, exif }) {
     let time = parseShotTime(exif).format("YYYY年MM月DD日 HH:mm:ss");
+    let transform = "";
+    switch (exif.Orientation) {
+        case 2:
+            transform = "rotateY(180deg)";
+            break;
+        case 3:
+            transform = "rotate(180deg)";
+            break;
+        case 4:
+            transform = "rotate(180deg)rotateY(180deg)";
+            break;
+        case 5:
+            transform = "rotate(270deg)rotateY(180deg)";
+            break;
+        case 6:
+            transform = "rotate(90deg)";
+            break;
+        case 7:
+            transform = "rotate(90deg)rotateY(180deg)";
+            break;
+        case 8:
+            transform = "rotate(270deg)";
+            break;
+    }
     return (
-        <img style={{ width: "100%" }} src={"http://127.0.0.1:1123/thumbnail?size=256&cutSquare=true&hash=" + hash} loading="lazy"
+        <img style={{ width: "100%", transform }} src={"http://127.0.0.1:1123/thumbnail?size=256&cutSquare=true&hash=" + hash} loading="lazy"
             title={time + " " + exif.OffsetTime + "\n"
                 + (exif.Model ? (exif.Model + "\n") : "")
                 + (exif.GPSLatitudeRef ? (exif.GPSLatitudeRef + " " + exif.GPSLatitude + "\n") : "")
