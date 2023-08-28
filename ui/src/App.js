21038cb4d1087c6390b9a9c8abc71afdd49db2b7
Xu Liang <isxuliang@gmail.com>
2023-08-28 21:05:10 +0800

    Drawer

--- ui/src/App.js
+++ ui/src/App.js
@@ -2,54 +2,127 @@ import Menu from 'components/Menu/Menu';
 import SystemConfig from 'pages/Setting/SystemConfig';
 import Version from 'components/Version';
 import Files from "./pages/Files";
-import {Box, Stack, useColorScheme} from "@mui/material";
-import React, {useEffect} from "react";
+import { AppBar, Box, Divider, Drawer, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Stack, Toolbar, Typography, styled, useColorScheme } from "@mui/material";
+import React, { useEffect } from "react";
 import useMenu from "./hox/menu";
 import useSysConfig from "./hox/sysConfig";
 import BackupTask from "./pages/BackupTask";
 import Dcim from 'pages/Dcim';
+import SvgIcon from 'components/Icon/SvgIcon';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Inbox, Mail } from '@mui/icons-material';
 
 function App() {
-    const {sysConfig} = useSysConfig();
-    const {menu} = useMenu();
-    const {mode, setMode} = useColorScheme();
+    const { sysConfig } = useSysConfig();
+    const { menu, setMenu } = useMenu();
+    const { mode, setMode } = useColorScheme();
+    const [open, setOpen] = React.useState(false);
+    const toggleDrawer = (open) => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+
+        setOpen(open);
+    };
     useEffect(() => {
         // document.body.setAttribute('data-theme', sysConfig.theme);
         console.log("mode:", mode, "=>", sysConfig.theme);
         setMode(sysConfig.theme);
     }, [sysConfig.theme]);
+    const DrawerHeader = styled('div')(({ theme }) => ({
+        display: 'flex',
+        alignItems: 'center',
+        padding: theme.spacing(0, 1),
+        // necessary for content to be below app bar
+        ...theme.mixins.toolbar,
+        justifyContent: 'flex-end',
+    }));
     return (
-        <Stack sx={{width: "100%", height: "100%", position: "fixed"}} direction="row">
-            <Box sx={{backgroundColor: theme => theme.background.secondary, color: theme => theme.context.secondary}}>
-                <Menu items={[
-                    {icon: 'wangpan', name: '我的云盘'},
-                    {icon: 'DCIM', name: '我的相册'},
-                    {icon: 'devices', name: '设备列表'},
-                    {icon: '', name: '文件类型'},
-                    {icon: '', name: '文件大小'},
-                    {icon: 'yuntongbu', name: '备份任务'},
-                    {icon: 'peizhi', name: '设置'},
+        <Stack sx={{ width: "100%", height: "100%", position: "fixed" }} direction="row">
+            <AppBar position="fixed" open={open}>
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={() => setOpen(true)}
+                        edge="start"
+                        sx={{
+                            marginRight: 5,
+                            ...(open && { display: 'none' }),
+                        }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" noWrap component="div">
+                        {menu}
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                anchor="left"
+                open={open}
+                onClose={toggleDrawer(false)}
+            >
+                <Box
+                    sx={{ width: 250 }}
+                    role="presentation"
+                    onClick={toggleDrawer(false)}
+                    onKeyDown={toggleDrawer(false)}
+                >
+                    <List>
+                        {[
+                            { icon: 'wangpan', name: '我的云盘' },
+                            { icon: 'DCIM', name: '我的相册' },
+                            { icon: 'devices', name: '设备列表' },
+                            { icon: '', name: '文件类型' },
+                            { icon: '', name: '文件大小' },
+                            { icon: 'yuntongbu', name: '备份任务' },
+                            { icon: 'peizhi', name: '设置' },
 
-                    {icon: 'swapVertical', name: '传输列表'},
-                    {icon: 'system', name: '资源监控'},
-                    {icon: '', name: '我的书签'},
-                    {icon: '', name: '分享历史'},
-                    {icon: '', name: '操作历史'},
-                    {icon: '', name: '搜索'},
-                ]}
-                />
-                <Version/>
-            </Box>
+                            { icon: 'swapVertical', name: '传输列表' },
+                            { icon: 'system', name: '资源监控' },
+                            { icon: '', name: '我的书签' },
+                            { icon: '', name: '分享历史' },
+                            { icon: '', name: '操作历史' },
+                            { icon: '', name: '搜索' },
+                        ].map((item, index) => (
+                            <ListItem key={item.name} disablePadding onClick={() => setMenu(item.name)}>
+                                <ListItemButton>
+                                    <ListItemIcon>
+                                        <SvgIcon icon={item.icon} />
+                                    </ListItemIcon>
+                                    <ListItemText primary={item.name} />
+                                </ListItemButton>
+                            </ListItem>
+                        ))}
+                    </List>
+                    <Divider />
+                    <List>
+                        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                            <ListItem key={text} disablePadding>
+                                <ListItemButton>
+                                    <ListItemIcon>
+                                        {index % 2 === 0 ? <Inbox /> : <Mail />}
+                                    </ListItemIcon>
+                                    <ListItemText primary={text} />
+                                </ListItemButton>
+                            </ListItem>
+                        ))}
+                    </List>
+                </Box>
+                <Version />
+            </Drawer>
             <Box sx={{
                 flex: 1,
                 position: "relative",
                 backgroundColor: theme => theme.background.primary,
                 color: theme => theme.context.primary
             }}>
-                <Files show={menu === '我的云盘'}/>
-                <Dcim show={menu === '我的相册'}/>
-                <BackupTask show={menu === '备份任务'}/>
-                <SystemConfig show={menu === '设置'}/>
+                <DrawerHeader />
+                <Files show={menu === '我的云盘'} />
+                <Dcim show={menu === '我的相册'} />
+                <BackupTask show={menu === '备份任务'} />
+                <SystemConfig show={menu === '设置'} />
             </Box>
         </Stack>
     );
