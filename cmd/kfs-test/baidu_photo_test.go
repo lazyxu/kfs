c1b7860d2d39eec47cef7ef3e7d2634102811852
Xu Liang <isxuliang@gmail.com>
2023-09-21 00:03:16 +0800

    kfs-test for github.com/alist-org/alist/v3/drivers/baidu_photo

---
+++ cmd/kfs-test/baidu_photo_test.go
@@ -0,0 +1,20 @@
+package kfs_test
+
+import (
+	"context"
+	"github.com/alist-org/alist/v3/cmd"
+	baidu_photo "github.com/alist-org/alist/v3/drivers/baidu_photo"
+	"testing"
+)
+
+func TestBaiduPhoto(t *testing.T) {
+	// GOPROXY=https://proxy.golang.com.cn,direct;
+	// export GOSUMDB=off
+	cmd.Init()
+
+	var baiduPhoto baidu_photo.BaiduPhoto
+	baiduPhoto.ClientID = "iYCeC9g08h5vuP9UqvPHKKSVrKFXGa1v"
+	baiduPhoto.ClientSecret = "jXiFMOPVPCWlO2M5CwWQzffpNPaGTRBG"
+	baiduPhoto.RefreshToken = "122.238075bc689e8f77bc5388db7991737c.YGu622hbpSoEQh1l4eZx_h87G1BCbqZp60BPXHQ.1tG0sg"
+	baiduPhoto.Init(context.TODO())
+}
