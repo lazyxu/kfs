3dba48169d34b0a84d862baac1ce87af87988b40
Xu Liang <isxuliang@gmail.com>
2023-12-07 21:22:40 +0800

    openAppByExt

--- ui/src/hox/windows.js
+++ ui/src/hox/windows.js
@@ -3,28 +3,28 @@ import { useState } from 'react';
 
 export const [useWindows, getWindows] = createGlobalStore(() => useState({
   "1": {
-      "id": 1,
-      "app": "APP_TEXT_VIEWER",
-      "props": {
-          "driver": {
-              "id": 1,
-              "name": "人格",
-              "description": "",
-              "type": "localFile",
-              "sync": false,
-              "h": 0,
-              "m": 0,
-              "accessToken": "",
-              "refreshToken": "",
-              "deviceId": 0,
-              "srcPath": "",
-              "ignores": "",
-              "encoder": ""
-          },
-          "filePath": [
-              "0.txt"
-          ]
-      }
+    "id": 1,
+    "app": "APP_TEXT_VIEWER",
+    "props": {
+      "driver": {
+        "id": 1,
+        "name": "人格",
+        "description": "",
+        "type": "localFile",
+        "sync": false,
+        "h": 0,
+        "m": 0,
+        "accessToken": "",
+        "refreshToken": "",
+        "deviceId": 0,
+        "srcPath": "",
+        "ignores": "",
+        "encoder": ""
+      },
+      "filePath": [
+        "0.txt"
+      ]
+    }
   }
 }));
 
@@ -55,3 +55,34 @@ export function closeWindow(setWindows, id) {
 export const APP_IMAGE_VIEWER = "APP_IMAGE_VIEWER";
 export const APP_VIDEO_VIEWER = "APP_VIDEO_VIEWER";
 export const APP_TEXT_VIEWER = "APP_TEXT_VIEWER";
+
+const appExtensions = {
+  APP_TEXT_VIEWER: [".txt", ".md", ".log",
+    ".go", ".js", ".jsx", ".java", ".c", ".h", ".c++", ".cpp"
+  ],
+  APP_IMAGE_VIEWER: [".jpg", ".jpeg", ".png", ".heic"],
+  APP_VIDEO_VIEWER: [".mp4", ".mov"],
+}
+
+const defaultApp = {
+}
+
+let openAppByExt = {}
+
+export function refreshOpenApp() {
+  for (const app in appExtensions) {
+    const extensions = appExtensions[app];
+    for (const ext of extensions) {
+      openAppByExt[ext] = app;
+    }
+  }
+}
+
+refreshOpenApp();
+
+export function getOpenApp(name) {
+  const ext = name.substring(name.lastIndexOf(".")).toLowerCase();
+  const app = openAppByExt[ext];
+  console.log("===getOpenApp", name, ext, openAppByExt, app);
+  return app;
+}
--- ui/src/pages/Files/DriverFiles/File/FileIcon.jsx
+++ ui/src/pages/Files/DriverFiles/File/FileIcon.jsx
@@ -1,8 +1,8 @@
 import { Box } from "@mui/material";
-import { isImage, isVideo, isViewable, modeIsDir } from "api/utils/api";
+import { modeIsDir } from "api/utils/api";
 import useResourceManager, { openDir } from "hox/resourceManager";
 import { getSysConfig } from "hox/sysConfig";
-import useWindows, { APP_IMAGE_VIEWER, APP_TEXT_VIEWER, APP_VIDEO_VIEWER, newWindow } from "hox/windows";
+import useWindows, { APP_IMAGE_VIEWER, APP_TEXT_VIEWER, APP_VIDEO_VIEWER, getOpenApp, newWindow } from "hox/windows";
 import { memo, useState } from "react";
 import SvgIcon from "../../../../components/Icon/SvgIcon";
 import ImgCancelable from "./ImgCancelable";
@@ -15,29 +15,25 @@ export default memo(({ driver, filePath, driverFile, hasBeenInView, inView }) =>
     const [windows, setWindows] = useWindows();
     const { name, mode } = driverFile;
     // console.log("===render", filePath, hasBeenInView)
-    return (
-        <Box className="file-icon-box">
-            {modeIsDir(mode) ?
+    if (modeIsDir(mode)) {
+        return (
+            <Box className="file-icon-box">
                 <SvgIcon icon="folder1" className='file-icon file-icon-folder' fontSize="inherit" onClick={() => {
                     openDir(setResourceManager, driver, filePath);
-                }} /> :
-                isImage(name) ?
-                    <ImgCancelable src={`${sysConfig.webServer}/thumbnail?size=64&hash=${driverFile.hash}`} inView={inView} onClick={() => {
-                        newWindow(setWindows, APP_IMAGE_VIEWER, { driver, filePath });
-                    }} /> :
-                    isVideo(name) ?
-                        <ImgCancelable src={`${sysConfig.webServer}/thumbnail?size=64&hash=${driverFile.hash}`} inView={inView} onClick={() => {
-                            newWindow(setWindows, APP_VIDEO_VIEWER, { driver, filePath });
-                        }} />
-                        : name.toLowerCase().endsWith(".txt") ?
-                            <SvgIcon icon="txt3" className='file-icon file-icon-file file-icon-file-viewable' fontSize="inherit" onClick={() => {
-                                newWindow(setWindows, APP_TEXT_VIEWER, { driver, filePath });
-                            }} />
-                            : isViewable(name) ?
-                                <SvgIcon icon="file12" className='file-icon file-icon-file file-icon-file-viewable' fontSize="inherit" onClick={() => {
-                                    newWindow(setWindows, APP_TEXT_VIEWER, { driver, filePath });
-                                }} />
-                                : <SvgIcon icon="file12" className='file-icon file-icon-file' fontSize="inherit" />
+                }} />
+            </Box>
+        );
+    }
+    const app = getOpenApp(name);
+    const onClick = () => newWindow(setWindows, app, { driver, filePath });
+    return (
+        <Box className="file-icon-box">
+            {
+                app === APP_IMAGE_VIEWER || app === APP_VIDEO_VIEWER ?
+                    <ImgCancelable src={`${sysConfig.webServer}/thumbnail?size=64&hash=${driverFile.hash}`} inView={inView} onClick={onClick} /> :
+                    app === APP_TEXT_VIEWER ?
+                        <SvgIcon icon="txt3" className='file-icon file-icon-file file-icon-file-viewable' fontSize="inherit" onClick={onClick} />
+                        : <SvgIcon icon="file12" className='file-icon file-icon-file' fontSize="inherit" />
             }
         </Box>
     );
