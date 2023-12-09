b076475494912f7c8cd84fc3342b1800da33c068
Xu Liang <isxuliang@gmail.com>
2023-12-09 12:38:38 +0800

    auto width for image in DCIM

--- ui/src/pages/Dcim/All.jsx
+++ ui/src/pages/Dcim/All.jsx
@@ -1,11 +1,32 @@
 import { Grid } from "@mui/material";
+import { useEffect, useRef, useState } from "react";
 import Image from "./Image";
 
+function calImageWith(gridWith) {
+    const n = gridWith / 100;
+    return gridWith / Math.ceil(n);
+}
+
 export default function ({ metadataList }) {
+    const ref = useRef(null);
+
+    const [width, setWidth] = useState(0);
+
+    useEffect(() => {
+        setWidth(calImageWith(ref.current.offsetWidth));
+        const onResize = () => {
+            console.log("resize")
+            setWidth(calImageWith(ref.current.offsetWidth));
+        };
+        window.addEventListener("resize", onResize);
+        return () => {
+            window.removeEventListener("resize", onResize);
+        }
+    }, []);
     return (
-        <Grid container padding={1} spacing={1}>
-            {metadataList.map(metadata => {
-                return <Grid item style={{ width: "256px", height: "256px" }} key={metadata.hash}>
+        <Grid container ref={ref} sx={{ width: "100%" }}>
+            {width && metadataList.map(metadata => {
+                return <Grid item style={{ width: width, height: width }} key={metadata.hash}>
                     <Image metadata={metadata} />
                 </Grid>
             })}
