5201afacbc55729f995fbd170706f4db33bf954c
Xu Liang <isxuliang@gmail.com>
2023-12-14 23:27:21 +0800

    baiduPhoto.png for electron build

--- ui/web/processManager.js
+++
@@ -1,9 +0,0 @@
-let processes = [];
-
-module.exports = {
-  getProcesses: () => processes,
-  addProcess: p => processes.push(p),
-  removeProcess: p => {
-    processes = processes.filter(v => v !== p);
-  }
-};
