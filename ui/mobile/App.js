3c69a0b5778d41c234e57829eb525c1f6f7551b9
Xu Liang <isxuliang@gmail.com>
2023-12-19 20:12:24 +0800

    try BottomNavigation

--- ui/mobile/App.js
+++ ui/mobile/App.js
@@ -1,39 +1,75 @@
 import { httpGet } from '@kfs/common/api/webServer';
 import useSysConfig from '@kfs/common/hox/sysConfig';
-import { StatusBar } from 'expo-status-bar';
 import { HoxRoot } from "hox";
 import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { BottomNavigation, PaperProvider } from 'react-native-paper';
 import Toast from 'react-native-toast-message';
 import "./global";
 import Photos from './pages/Photos';
 
-function App1() {
-  const { sysConfig, setSysConfig } = useSysConfig();
+const MusicRoute = () => <Text>Music</Text>;
+
+const AlbumsRoute = () => {
   let [drivers, setDrivers] = useState([]);
-  console.log("sysConfig", sysConfig);
   console.log("drivers", drivers);
   useEffect(() => {
     httpGet("/api/v1/drivers").then(setDrivers);
   }, []);
   return (
     <View style={styles.container}>
-      <Text>App.js!</Text>
-      <Text>{JSON.stringify(sysConfig, undefined, 2)}</Text>
       {drivers.map(driver => (
         <Text key={driver.name}>{driver.name}</Text>
       ))}
-      <Photos />
-      <StatusBar style="auto" />
-      <Toast />
     </View>
+  );
+};
+
+const RecentsRoute = () => <Text>Recents</Text>;
+
+const NotificationsRoute = () => {
+  const { sysConfig, setSysConfig } = useSysConfig();
+  console.log("sysConfig", sysConfig);
+  return (
+    <View style={styles.container}>
+      <Text>{JSON.stringify(sysConfig, undefined, 2)}</Text>
+    </View>
+  );
+};
+
+function App1() {
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
+    { key: 'music', title: 'Favorites', focusedIcon: 'heart', unfocusedIcon: 'heart-outline' },
+    { key: 'albums', title: 'Albums', focusedIcon: 'album' },
+    { key: 'recents', title: 'Recents', focusedIcon: 'history' },
+    { key: 'notifications', title: 'Notifications', focusedIcon: 'bell', unfocusedIcon: 'bell-outline' },
+  ]);
+
+  const renderScene = BottomNavigation.SceneMap({
+    music: Photos,
+    albums: AlbumsRoute,
+    recents: RecentsRoute,
+    notifications: NotificationsRoute,
+  });
+
+  return (
+    <BottomNavigation
+      navigationState={{ index, routes }}
+      onIndexChange={setIndex}
+      renderScene={renderScene}
+      shifting={true}
+    />
   );
 }
 
 export default function App() {
   return (
     <HoxRoot>
-      <App1 />
+      <PaperProvider>
+        <App1 />
+        <Toast />
+      </PaperProvider>
     </HoxRoot>
   );
 }
@@ -44,5 +80,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    overflow: "scroll"
   },
 });
