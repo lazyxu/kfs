47ba76d93283290c86125ccd015db2d0570c55ea
Xu Liang <isxuliang@gmail.com>
2023-12-30 01:14:51 +0800

    add useInView

--- ui/mobile/pages/Photos/Thumbnail.jsx
+++ ui/mobile/pages/Photos/Thumbnail.jsx
@@ -1,15 +1,24 @@
+import ImgCancelable from '@kfs/common/components/ImgCancelable';
 import { getSysConfig } from "@kfs/common/hox/sysConfig";
-import { Pressable } from 'react-native';
+import { useInView } from "react-intersection-observer";
+import { Pressable, View } from 'react-native';
 import FastImage from './FastImage';
 
 export default function ({ hash, width, navigation }) {
-    // const { ref, inView } = useInView({ threshold: 0 });
+    const { ref, inView } = useInView({ threshold: 0 });
+    const src = `${getSysConfig().webServer}/thumbnail?size=256&cutSquare=true&hash=${hash}`;
     return (
-        <Pressable onPress={() => navigation.navigate("Viewer", { hash })}  >
-            <FastImage style={{
+        <Pressable ref={ref} onPress={() => navigation.navigate("Viewer", { hash })}  >
+            <ImgCancelable inView={inView}
+                src={src}
+                renderImg={(url) => <FastImage style={{
                     width: width,
                     height: width,
-                }} source={{ uri: `${getSysConfig().webServer}/thumbnail?size=256&cutSquare=true&hash=${hash}` }} 
+                }} source={{ uri: url }} />}
+                renderSkeleton={() => <View style={{
+                    width: width,
+                    height: width,
+                }} />}
             />
         </Pressable>
     );
