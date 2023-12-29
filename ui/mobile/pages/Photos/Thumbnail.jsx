5c05e48bfa55abf80934989683b5427a63e43663
Xu Liang <isxuliang@gmail.com>
2023-12-29 23:14:33 +0800

    FastImage

--- ui/mobile/pages/Photos/Thumbnail.jsx
+++ ui/mobile/pages/Photos/Thumbnail.jsx
@@ -1,11 +1,12 @@
 import { getSysConfig } from "@kfs/common/hox/sysConfig";
-import { Image, Pressable } from 'react-native';
+import { Pressable } from 'react-native';
+import FastImage from './FastImage';
 
 export default function ({ hash, width, navigation }) {
     // const { ref, inView } = useInView({ threshold: 0 });
     return (
         <Pressable onPress={() => navigation.navigate("Viewer", { hash })}  >
-            <Image style={{
+            <FastImage style={{
                     width: width,
                     height: width,
                 }} source={{ uri: `${getSysConfig().webServer}/thumbnail?size=256&cutSquare=true&hash=${hash}` }} 
