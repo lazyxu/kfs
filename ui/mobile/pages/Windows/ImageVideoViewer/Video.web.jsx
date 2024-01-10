9965f3c68fa3bc96bcd995031455d0dbb1e83742
Xu Liang <isxuliang@gmail.com>
2024-01-10 20:32:34 +0800

    load video when source change

--- ui/mobile/pages/Windows/ImageVideoViewer/Video.web.jsx
+++ ui/mobile/pages/Windows/ImageVideoViewer/Video.web.jsx
@@ -1,6 +1,12 @@
+import { useEffect, useRef } from 'react';
+
 export default function ({ width, height, source }) {
+    const ref = useRef();
+    useEffect(() => {    
+        ref.current?.load();
+    }, [source]);
     return (
-        <video controls style={{ width, height }} data-setup='{}'>
+        <video ref={ref} controls style={{ width, height }} data-setup='{}'>
             <source src={source} />
             您的浏览器不支持 HTML5 video 标签。
         </video>
