5325af7658bd5322151eae55b363b97f6dd9bd41
Xu Liang <isxuliang@gmail.com>
2024-01-29 22:18:17 +0800

    allows running multiple containers for kfs-server

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,25 @@
 FROM ubuntu:22.04
 
-CMD ["/root/code/kfs/cmd/kfs-server/kfs-server"]
+ADD cmd/kfs-server/kfs-server /kfs-server
+
+WORKDIR /
+
+ENV KFS_ROOT /root/kfs
+ENV WebServer 1123
+ENV SocketServer 1124
+ENV DatabaseType sqlite
+ENV DataSourceNameStr $KFS_ROOT/sqlite.db
+ENV StorageType 1
+ENV StorageDir $KFS_ROOT
+ENV ThumbnailDir $KFS_ROOT/thumbnail
+ENV TransCodeDir $KFS_ROOT/transcode
+
+CMD /kfs-server \
+    --web-server $WebServer \
+    --socket-server $SocketServer \
+    --database-type $DatabaseType \
+    --data-source-name $DataSourceNameStr \
+    --storage-type $StorageType \
+    --storage-dir $StorageDir \
+    --thumbnail-dir $ThumbnailDir \
+    --transcode-dir $TransCodeDir
--- docker.sh
+++ docker.sh
@@ -4,7 +4,11 @@ imeage_name=kfs-server
 docker rm -f ${imeage_name} || true
 docker images | grep ${imeage_name} | awk '{printf "%s:%s ",$1,$2}' | xargs docker rmi || true
 image_tag=`date '+%Y%m%d_%H%M%S'`
+mkdir -p .kfs-server
 docker build -t ${imeage_name}:${image_tag} .
+docker tag ${imeage_name}:${image_tag} ${imeage_name}:latest
 docker run --privileged=true -p 1123:1123 -p 1124:1124 --name ${imeage_name} \
-    -v ~/code:/root/code -w /root/code/kfs -d \
+    -v `pwd`/.kfs:/root/kfs -w /root/kfs --restart=always -d \
     ${imeage_name}:${image_tag}
+
+# docker run --privileged=true -p 1125:1123 -p 1126:1124 --name kfs-server-test -v /.kfs:/root/kfs -w /root/kfs --restart=always -d kfs-server:latest
